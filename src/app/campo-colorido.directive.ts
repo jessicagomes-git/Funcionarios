cd448cbfbf57cbcd14fee193c9b0a05ad4440fdd
jessicagomes-git <jessicagomes802@gmail.com>
2021-01-18 20:28:34 -0300

    Melhorando o codigo da diretiva

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,24 +1,26 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appCampoColorido]'
+  selector: '[appCampoColorido]',
+  exportAs: 'campoColorido'
 })
 export class CampoColoridoDirective {
 
-  constructor(
-    private elementRef: ElementRef, //Objeto que da acesso ao elemento da DOM
-    private renderer: Renderer2 //Serviço que fornece abstração para manipular redenrização(Manipulação de elementos)
-  ) {  }
+  //Fazendo um vinculo
+  @HostBinding('style.backgroundColor') corDeFundo: string;
+  @Input() cor = 'gray';
+
+  constructor() {  }
   //Pegando o elemento nativo do html
 
   //Anotação para indicar o ganho de foc0 com o focos
-  @HostListener('focus') QuandoDerFoco(){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'yellow');
+  @HostListener('focus') colorir(){
+    this.corDeFundo = this.cor;
   }
 
   //Anotação para indicar a perda de foc0 com o blur
-  @HostListener('blur') aoPerderFoco(){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  @HostListener('blur') descolorir(){
+    this.corDeFundo = 'transparent';
   }
 
 }
